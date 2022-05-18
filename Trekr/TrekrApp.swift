2643570f7e35f4dec4c04e6cd0989fb95691c70e
Shahad Alfakhri <shahad.alfakhri@pwc.com>
2022-05-18 23:34:08 +0200

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Shahad Alfakhri on 18.05.22.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
